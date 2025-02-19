671062c7b674b43049e9332428b515c6c15ac719
Carlos <carlos_aperdomo@soy.sena.edu.co>
2025-02-18 23:18:54 -0500

    combinar cambios

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,9 @@ function App() {
   return (
     <>
       <div>
-<<<<<<< HEAD
-      <h1 class="text-3xl font-bold underline text-red-500">Quiero poner la letra roja</h1>
-
-       
-=======
         <Header />
         <AppRouter />
->>>>>>> dc24f67 (router y header)
+
       </div>
 
     </>
